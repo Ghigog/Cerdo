fb401241e853876fd41d15d627ef05b2d2f05ecb
Fran <fran@pop-os.default>
2024-06-05 21:02:52 -0500

    Add dice roll logic and hold logic

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,11 +1,57 @@
 extends Control
 
+@onready var current_score_label = $CanvasLayer/VBoxContainer/HBoxContainer2/CurrentScoreLabel
+@onready var player_turn_h_box_container = $CanvasLayer/VBoxContainer/PlayerTurnHBoxContainer
+@onready var dice_texture = $CanvasLayer/VBoxContainer/CenterContainer/DiceTexture
+
+@onready var player_1_score_label = $CanvasLayer/VBoxContainer/HBoxContainer/Player1ScoreLabel
+@onready var player_2_score_label = $CanvasLayer/VBoxContainer/HBoxContainer/Player2ScoreLabel
+
+
+var dice_value: int = 0
+var current_score: int = 0
+
+var random = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	random.randomize()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_back_pressed():
+	pass # Replace with function body.
+
+func _on_roll_button_pressed():
+	dice_value = random.randi_range(1, 6)
+	var folder = "res://Assets/DiceImages/"
+	var filename = "dice" + str(dice_value) + ".svg"
+	dice_texture.texture = load(folder + filename)
+	print(dice_value)
+	if dice_value == 1:
+		_swap_players()
+	else:
+		current_score += dice_value
+		current_score_label.text = str(current_score)
+
+func _on_hold_button_pressed():
+	Global.player_scores[Global.turn] += current_score
+	player_1_score_label.text = str(Global.player_scores[0])
+	player_2_score_label.text = str(Global.player_scores[1])
+	if Global.player_scores[Global.turn] >= 100:
+		print("TODO: move to end screen")
+		print("omg you did congrats :0000")
+	_swap_players()
+
+func _swap_players():
+	Global.turn = 1 if Global.turn == 0 else 0
+	current_score = 0
+	current_score_label.text = str(current_score)
+	player_turn_h_box_container.alignment = (
+		player_turn_h_box_container.ALIGNMENT_BEGIN
+		if Global.turn == 0 else
+		player_turn_h_box_container.ALIGNMENT_END
+	)
