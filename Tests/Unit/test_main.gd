45cb75f5e4b9371256f015444f68a9911ab25008
Dylan Growcoot <dylangrowcoot@gmail.com>
2024-06-11 21:32:12 -0500

    Added unit test for refreshing scores

--- Tests/Unit/test_main.gd
+++ Tests/Unit/test_main.gd
@@ -46,3 +46,13 @@ func test_refresh_turn_1_from_1():
 	before_each_refresh_turn()
 	refresh_turn_from_to( _main.player_turn_h_box_container.ALIGNMENT_END, 
 	1, _main.player_turn_h_box_container.ALIGNMENT_END )
+
+func test_render_scores():
+	Global.player_scores = [7,8]
+	_main.player_1_score_label = double(Label).new()
+	_main.player_2_score_label = double(Label).new()
+	_main.player_1_score_label.text = "randomtext"
+	_main.player_2_score_label.text = "randomtext2"
+	_main.render_scores();
+	assert_eq(_main.player_1_score_label.text, str(Global.player_scores[0]), "Expected player 1 score to be: " + str(Global.player_scores[0]))
+	assert_eq(_main.player_2_score_label.text, str(Global.player_scores[1]), "Expected player 2 score to be: " + str(Global.player_scores[1]))
