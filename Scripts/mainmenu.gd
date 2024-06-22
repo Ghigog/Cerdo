c4d66b0949e9d581a9ce129d6177772712c2a860
Nicholas Spalding <nishedcob@users.noreply.github.com>
2024-06-22 11:36:17 -0500

    Fix loose read in main menu

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ready
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
