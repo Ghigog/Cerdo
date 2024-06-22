8ee7306cd10f2e1a967c8f212a31c3cf24c4d4d3
Nicholas Spalding <nishedcob@users.noreply.github.com>
2024-06-22 12:11:10 -0500

    Disable Continue Button when there is no game in progress

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -1,8 +1,15 @@
 extends Control
 
+@onready var continue_button = $CanvasLayer/VBoxContainer/ContinueButton
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var save_exists = Global.save_game_exists()
+	if !save_exists:
+		continue_button.disabled = true
+	else:
+		Global.load_variables(false)
+		continue_button.disabled = !Global.is_game_in_progress
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
